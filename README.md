ce9aae485f56c8f658c67e9ef896a6ada53b01d4
Alve Svarén <alve@hotmail.se>
2024-11-28 12:17:19 +0100

    Add readme and clean up code

---
+++ README.md
@@ -0,0 +1,11 @@
+Issue: https://github.com/pyodide/pyodide/issues/5232
+
+# To run
+
+1. Clone the repository
+1. Run `pnpm install`
+1. Run `pnpm dev`
+1. Open `http://localhost:5173` in your browser
+1. See in the console that "Hello world" is printed
+1. Press the "Send interrupt" button
+1. See that "Hello world" continues to be printed for a while after the "Cancelling worker" message
--- src/worker.ts
+++ src/worker.ts
@@ -4,20 +4,17 @@ console.log("Worker loaded");
 let pyodide = await loadPyodide();
 console.log("Pyodide loaded");
 
-self.postMessage({type: "loaded"});
+self.postMessage({ type: "loaded" });
 
 self.addEventListener("message", async e => {
   console.log("Set interrupt buffer");
   pyodide.setInterruptBuffer(e.data);
   pyodide.setStdout({
     batched(output) {
-      self.postMessage({type: 'stdout', output});
+      self.postMessage({ type: "stdout", output });
     },
-  })
+  });
 
-  pyodide.runPythonAsync(`
-
-while 1:
-  print("Hello world")
-`)
+  pyodide.runPythonAsync(`while 1:
+  print("Hello world")`);
 });
